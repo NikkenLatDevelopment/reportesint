2fb0eb8a2eda25dec2890c3fad2fffa224959065
Fmelchor Nikken <fmelchor@nikkenlatam.com>
2024-06-26 19:29:00 -0600

    Update reportesRetos.php

--- app/Http/Controllers/reportesRetos/reportesRetos.php
+++ app/Http/Controllers/reportesRetos/reportesRetos.php
@@ -28,7 +28,7 @@ class reportesRetos extends Controller{
         $timeCaching = 4680; #in seconds: 86400 = 24 horas, 43200 = 12 horas, 21600 = 6 horas, 14400 = 4 horas, 7200 = 2 horas, 4680 = 1 hora 30 minutos, 3600 = 1 hora
         
         $data = cache()->remember($key, $timeCaching, static function () use ($periodo, $coreApp) {
-            $data = $coreApp->execSQLQuery("SELECT Associateid, AssociateName,Rango, Pais, PV AS 'VP' ,GV AS 'VGP' ,OV AS 'VO',QOVOPL AS 'VO_LDP',QOVOPSL AS 'VO_LDPyS',Period AS 'Periodo',Sponsorid, SponsorName,SponsorPais, AssociateType, Estatus, estado FROM [LAT_MyNIKKEN].dbo.VolumeGlob with(nolock) WHERE Period = $periodo AND ltrim(rtrim(associateid)) LIKE '%03' AND associatetype=100", 'SQL173');
+            $coreApp->execSQLQuery("SELECT Associateid, AssociateName,Rango, Pais, PV AS 'VP' ,GV AS 'VGP' ,OV AS 'VO',QOVOPL AS 'VO_LDP',QOVOPSL AS 'VO_LDPyS',Period AS 'Periodo',Sponsorid, SponsorName,SponsorPais, AssociateType, Estatus, estado FROM [LAT_MyNIKKEN].dbo.VolumeGlob with(nolock) WHERE Period = $periodo AND ltrim(rtrim(associateid)) LIKE '%03' AND associatetype=100", 'SQL173');
         });
         return new volumenGlobal($data);
     }
