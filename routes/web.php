d09a045b495afd42cfe3ecd19d21ae0213b3b385
Fmelchor Nikken <fmelchor@nikkenlatam.com>
2026-01-30 12:37:59 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('getReportSalesTv3DS', 'otros\otros@getReportSalesTv3DS');
 
 Route::get('reporte_fichados', 'otros\otros@ficha2');
 Route::get('fichaCongreso', 'otros\otros@fichaCongreso');
-// Route::get('getUsersTvPasswords', 'otros\otros@getUsersTvPasswords');
+Route::get('getUsersTvPasswords', 'otros\otros@getUsersTvPasswords');
 
 Route::get('/finAnioPlata', 'otros\otros@finAnioPlata');
 Route::get('/getFinAnioPlata', 'otros\otros@getFinAnioPlata');
