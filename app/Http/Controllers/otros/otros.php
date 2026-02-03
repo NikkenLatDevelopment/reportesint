dffa1cac6772d560c1c2a8568a0383dfcde6c0cd
Fmelchor Nikken <fmelchor@nikkenlatam.com>
2026-02-03 14:46:31 -0600

    Update otros.php

--- app/Http/Controllers/otros/otros.php
+++ app/Http/Controllers/otros/otros.php
@@ -27,42 +27,61 @@ class otros extends Controller
         $spreadsheet = new Spreadsheet();
 
         # hoja 1
-        $hoja1 = $spreadsheet->getActiveSheet();
+            $hoja1 = $spreadsheet->getActiveSheet();
 
-        $hoja1->setTitle("2024");
-        for ($i = 65; $i <= 90; $i++) {
-            $letter = chr($i);
-            $hoja1->getColumnDimension($letter)->setAutoSize(true);
-        }
-        $hoja1->getStyle('A3:N3')->getFont()->setBold(true);
+            $hoja1->setTitle("2024");
+            for ($i = 65; $i <= 90; $i++) {
+                $letter = chr($i);
+                $hoja1->getColumnDimension($letter)->setAutoSize(true);
+            }
+            $hoja1->getStyle('A3:N3')->getFont()->setBold(true);
 
-        $hoja1->mergeCells('A1:N1');
-        $hoja1->setCellValue('A1', "Estrategia CHL Rangos Ejecutivos | Fecha de actualización: " . Date("Y-m-d H:i:s"));
-        $hoja1->getStyle('A1')->getFont()->setBold(true);
+            $hoja1->mergeCells('A1:N1');
+            $hoja1->setCellValue('A1', "Estrategia CHL Rangos Ejecutivos | Fecha de actualización: " . Date("Y-m-d H:i:s"));
+            $hoja1->getStyle('A1')->getFont()->setBold(true);
 
-        $h = ['Código de Socio', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Cumple VP', 'Cumple VGP', 'Cumple', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
-        $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_EXE_ganadores_chl_gtm (202401,202412);", "SQL173", $h);
-        $hoja1->fromArray($d, null, 'A3', true);
+            $h = ['Código de Socio', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Cumple VP', 'Cumple VGP', 'Cumple', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
+            $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_EXE_ganadores_chl_gtm (202401,202412);", "SQL173", $h);
+            $hoja1->fromArray($d, null, 'A3', true);
         # hoja 1
 
         # hoja 2
-        $hoja2 = $spreadsheet->createSheet();
-        $hoja2->setTitle("2025");
+            $hoja2 = $spreadsheet->createSheet();
+            $hoja2->setTitle("2025");
 
-        for ($i = 65; $i <= 90; $i++) {
-            $letter = chr($i);
-            $hoja2->getColumnDimension($letter)->setAutoSize(true);
-        }
-        $hoja2->getStyle('A3:N3')->getFont()->setBold(true);
+            for ($i = 65; $i <= 90; $i++) {
+                $letter = chr($i);
+                $hoja2->getColumnDimension($letter)->setAutoSize(true);
+            }
+            $hoja2->getStyle('A3:N3')->getFont()->setBold(true);
 
-        $hoja2->mergeCells('A1:M1');
-        $hoja2->setCellValue('A1', "Estrategia CHL Rangos Ejecutivos | Fecha de actualización: " . Date("Y-m-d H:i:s"));
-        $hoja2->getStyle('A1')->getFont()->setBold(true);
+            $hoja2->mergeCells('A1:M1');
+            $hoja2->setCellValue('A1', "Estrategia CHL Rangos Ejecutivos | Fecha de actualización: " . Date("Y-m-d H:i:s"));
+            $hoja2->getStyle('A1')->getFont()->setBold(true);
 
-        $h = ['Código de Socio', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Cumple VP', 'Cumple VGP', 'Cumple', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
-        $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_EXE_ganadores_chl_gtm (202501,202512);", "SQL173", $h);
-        $hoja2->fromArray($d, null, 'A3', true);
+            $h = ['Código de Socio', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Cumple VP', 'Cumple VGP', 'Cumple', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
+            $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_EXE_ganadores_chl_gtm (202501,202512);", "SQL173", $h);
+            $hoja2->fromArray($d, null, 'A3', true);
         # hoja 2
+        
+        # hoja 6
+            $hoja3 = $spreadsheet->createSheet();
+            $hoja3->setTitle("2026");
+
+            for ($i = 65; $i <= 90; $i++) {
+                $letter = chr($i);
+                $hoja3->getColumnDimension($letter)->setAutoSize(true);
+            }
+            $hoja3->getStyle('A3:N3')->getFont()->setBold(true);
+
+            $hoja3->mergeCells('A1:M1');
+            $hoja3->setCellValue('A1', "Estrategia CHL Rangos Ejecutivos | Fecha de actualización: " . Date("Y-m-d H:i:s"));
+            $hoja3->getStyle('A1')->getFont()->setBold(true);
+
+            $h = ['Código de Socio', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Cumple VP', 'Cumple VGP', 'Cumple', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
+            $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_EXE_ganadores_chl_gtm (202601,202612);", "SQL173", $h);
+            $hoja3->fromArray($d, null, 'A3', true);
+        # hoja 6
 
         // Guardar el archivo temporalmente
         $tempFilePath = tempnam(sys_get_temp_dir(), 'export_');
@@ -91,41 +110,60 @@ class otros extends Controller
         $spreadsheet = new Spreadsheet();
 
         # hoja 1
-        $hoja1 = $spreadsheet->getActiveSheet();
+            $hoja1 = $spreadsheet->getActiveSheet();
 
-        $hoja1->setTitle("2024");
-        for ($i = 65; $i <= 90; $i++) {
-            $letter = chr($i);
-            $hoja1->getColumnDimension($letter)->setAutoSize(true);
-        }
-        $hoja1->getStyle('A3:N3')->getFont()->setBold(true);
+            $hoja1->setTitle("2024");
+            for ($i = 65; $i <= 90; $i++) {
+                $letter = chr($i);
+                $hoja1->getColumnDimension($letter)->setAutoSize(true);
+            }
+            $hoja1->getStyle('A3:N3')->getFont()->setBold(true);
 
-        $hoja1->mergeCells('A1:M1');
-        $hoja1->setCellValue('A1', "Estrategia CHL Lideres | Fecha de actualización: " . Date("Y-m-d H:i:s"));
-        $hoja1->getStyle('A1')->getFont()->setBold(true);
+            $hoja1->mergeCells('A1:M1');
+            $hoja1->setCellValue('A1', "Estrategia CHL Lideres | Fecha de actualización: " . Date("Y-m-d H:i:s"));
+            $hoja1->getStyle('A1')->getFont()->setBold(true);
 
-        $h = ['Código de Influencer', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Total de incorporaciones', 'Total Cumplen Estrategia', 'Cumple VP', 'Cumple VGP', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
-        $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_Lideres_ganadores_CHL_GTM (202401,202412);", "SQL173", $h);
-        $hoja1->fromArray($d, null, 'A3', true);
+            $h = ['Código de Influencer', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Total de incorporaciones', 'Total Cumplen Estrategia', 'Cumple VP', 'Cumple VGP', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
+            $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_Lideres_ganadores_CHL_GTM (202401,202412);", "SQL173", $h);
+            $hoja1->fromArray($d, null, 'A3', true);
         # hoja 1
 
         # hoja 2
-        $hoja2 = $spreadsheet->createSheet();
-        $hoja2->setTitle("2025");
+            $hoja2 = $spreadsheet->createSheet();
+            $hoja2->setTitle("2025");
 
-        for ($i = 65; $i <= 90; $i++) {
-            $letter = chr($i);
-            $hoja2->getColumnDimension($letter)->setAutoSize(true);
-        }
-        $hoja2->getStyle('A3:N3')->getFont()->setBold(true);
+            for ($i = 65; $i <= 90; $i++) {
+                $letter = chr($i);
+                $hoja2->getColumnDimension($letter)->setAutoSize(true);
+            }
+            $hoja2->getStyle('A3:N3')->getFont()->setBold(true);
 
-        $hoja2->mergeCells('A1:M1');
-        $hoja2->setCellValue('A1', "Estrategia CHL Lideres | Fecha de actualización: " . Date("Y-m-d H:i:s"));
-        $hoja2->getStyle('A1')->getFont()->setBold(true);
+            $hoja2->mergeCells('A1:M1');
+            $hoja2->setCellValue('A1', "Estrategia CHL Lideres | Fecha de actualización: " . Date("Y-m-d H:i:s"));
+            $hoja2->getStyle('A1')->getFont()->setBold(true);
 
-        $h = ['Código de Influencer', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Total de incorporaciones', 'Total Cumplen Estrategia', 'Cumple VP', 'Cumple VGP', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
-        $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_Lideres_ganadores_CHL_GTM (202501,202512);", "SQL173", $h);
-        $hoja2->fromArray($d, null, 'A3', true);
+            $h = ['Código de Influencer', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Total de incorporaciones', 'Total Cumplen Estrategia', 'Cumple VP', 'Cumple VGP', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
+            $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_Lideres_ganadores_CHL_GTM (202501,202512);", "SQL173", $h);
+            $hoja2->fromArray($d, null, 'A3', true);
+        # hoja 2
+        
+        # hoja 2
+            $hoja3 = $spreadsheet->createSheet();
+            $hoja3->setTitle("2026");
+
+            for ($i = 65; $i <= 90; $i++) {
+                $letter = chr($i);
+                $hoja3->getColumnDimension($letter)->setAutoSize(true);
+            }
+            $hoja3->getStyle('A3:N3')->getFont()->setBold(true);
+
+            $hoja3->mergeCells('A1:M1');
+            $hoja3->setCellValue('A1', "Estrategia CHL Lideres | Fecha de actualización: " . Date("Y-m-d H:i:s"));
+            $hoja3->getStyle('A1')->getFont()->setBold(true);
+
+            $h = ['Código de Influencer', 'Nombre', 'País', 'Rango', 'Periodo', 'VP Latam', 'VGP Latam', 'Total de incorporaciones', 'Total Cumplen Estrategia', 'Cumple VP', 'Cumple VGP', 'Cumple Incorporaciones', 'Código Patrocinador', 'Nombre patrocinador'];
+            $d = $core->getReportBody("SELECT * FROM RETOS_ESPECIALES.dbo.EstrategiaGTM_SLV_Lideres_ganadores_CHL_GTM (202601,202612);", "SQL173", $h);
+            $hoja3->fromArray($d, null, 'A3', true);
         # hoja 2
 
         // Guardar el archivo temporalmente
